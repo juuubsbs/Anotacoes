5ff27c83d3a8393ecbe9c996fb9cdacc8695561b
juuubsbs <julianasimon@edu.univali.br>
2024-06-16 19:25:01 -0300

    tá dando erro de winmain e eu to catando ainda

--- ex4/exercicio4.cpp
+++ ex4/exercicio4.cpp
@@ -9,7 +9,7 @@ int main()
     int array[x], i, positivo=0, negativo=0;    
     for( i = 0; i < x; i++)
     {
-        std:: cout << "Digite o valor de posição " << i + 1 << " : ";
+        std:: cout << "Digite o valor de posicao " << i + 1 << " : ";
         std:: cin >> array[i];
         if (array[i] < 0)
         {
