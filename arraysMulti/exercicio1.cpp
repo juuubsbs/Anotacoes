58f815b23dc49b8917941e52a5c320de058537ab
juuubsbs <julianasimon@edu.univali.br>
2024-06-17 15:21:26 -0300

    matriz contando bonitinho que funciona

--- arraysMulti/exercicio1.cpp
+++ arraysMulti/exercicio1.cpp
@@ -9,12 +9,15 @@ int main()
     std:: cout << " digite a quantidade de colunas de sua matriz: ";
     std:: cin >> y;
 
-    int matrix[x][y], i, j, par, impar;
+    int matrix[x][y], i, j, par = 0, impar = 0;
 
     for( i = 0; i < x; i++)
     {
         for( j = 0; j < y; j++)
         {
+            std:: cout << "digite o valor de linha " << i + 1 << " e coluna " << j + 1 << ": ";
+            std:: cin >> matrix[i][j];
+
             if ( matrix[i][j] % 2 != 0)
             {
                 impar++;
@@ -22,6 +25,9 @@ int main()
             else par++;
         }
     }
+
+    std:: cout << "A quantidade de numeros pares digitados foi: " << par << std:: endl;
+    std:: cout << "A quantidade de numeros impares digitados foi: " << impar << std:: endl;
 
     return 0;
 }
